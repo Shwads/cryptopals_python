4956c661850f0865ef6b961336ac8fdb8b04e927
Shwads <adam.rasool99@gmail.com>
2026-04-18 14:09:20 +0100

    Add file for bytearray handling functions.

--- cryptopals/utils/__init__.py
+++ cryptopals/utils/__init__.py
@@ -1,3 +1,4 @@
 from .hex import *
 from .base64 import *
 from .utils import *
+from .bytearrays import *
---
+++ cryptopals/utils/bytearrays.py
@@ -0,0 +1,2 @@
+def xor_bytearrays(ba1: bytearray, ba2: bytearray) -> bytearray:
+    pass
